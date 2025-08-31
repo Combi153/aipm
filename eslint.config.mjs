0a59875cd0ac5abbef1609e507ff68e508661806
Combi153 <jcm04@naver.com>
2025-08-31 12:57:12 +0900

    :technologist: Update ESLint configuration and add VSCode launch settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,8 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn',
